c497767066773fada9cefc14140d79b3df9e1e94
kduan005 <dk07071@hotmail.com>
2019-02-17 20:35:19 -0600

    build vm translator part1, comments added

--- project7/src/VMTranslator.py
+++ project7/src/VMTranslator.py
@@ -3,32 +3,46 @@ import collections
 
 class parser(object):
     '''
-    parser class that translate a single line of vm command into asm command
+    parser class that parse a single line of vm command into individual parts
+    including:
+    commandType: if a command is arithmetic or push/pop
+    argOne: first argument including "sub", "neg", "gt", "not",
+    "add", "eq", "lt", "or", "and", "not", "push", "pop"
+    argTwo: second argument, namely index into each memory segment, a command has
+    second argument only when the command type is push/pop
     '''
     def __init__(self):
-        self.raw = "" #raw vm command
-        self.tokens = [] #split raw command into tokens
+        #raw vm command
+        self.raw = ""
+        #split raw command into a list of tokens
+        self.tokens = []
         self.commandType = ""
         self.argOne = ""
         self.argTwo = ""
 
     def genCommandType(self):
-        if self.tokens[0] in {"sub", "neg", "gt", "not", "add", "eq", "lt", "or", "and", "not"}:
-            self.commandType = "ari"
+        #method to assign command type
+        if self.tokens[0] in {"sub", "neg", "gt", "not", "add", "eq", "lt", "or",\
+         "and", "not"}:
+            self.commandType = "arithmetic"
         else:
+            #push/pop
             self.commandType = self.tokens[0]
 
     def genArgOne(self):
-        if self.commandType == "ari":
+        #method to assign argOne according to command types
+        if self.commandType == "arithmetic":
             self.argOne = self.tokens[0]
         else:
             self.argOne = self.tokens[1]
 
     def genArgTwo(self):
+        #method to assign argTwo, only applicable when command type is push/pop
         if self.commandType in {"push", "pop"}:
             self.argTwo = self.tokens[2]
 
     def generate(self, vmCmd):
+        #generate commandType, argOne, argTwo for a single command line
         self.raw = vmCmd
         self.tokens = vmCmd.split(" ")
         self.genCommandType()
@@ -37,16 +51,28 @@ class parser(object):
 
 class codeWriter(object):
     '''
-    codeWriter class to translate each vm
+    codeWriter class to translate each vm command into assembly language command
+    by using commandType, argOne, argTwo information associated with each command
     '''
     def __init__(self, outname):
+        #take out filename from absolute/relative path and store it in self.outname
+        #it will be used for generate label name for static variables
         self.outname = outname.split("/")[-1]
+        #commandType, argOne, argTwo attributes have the same value with those
+        #associated with parser object
         self.commandType = ""
         self.argOne = ""
         self.argTwo = ""
+        #a list for storing assembly language commands for each vm command line
         self.asmCmd = []
+        #a counter for recording index of continue label in assembly language,
+        #used for generating continue labels for vm command of "eq", "lt", "gt"
         self.continue_counter = 0
+        #output file
         self.outf = open(outname + ".asm", "w")
+        #a directory mapping key words in vm command to corresponding key words
+        #in assembly language, default value set to be "self.outname.self.argTwo"
+        #for mapping static variables and its labels
         self.d = collections.defaultdict(lambda: self.outname + "." + self.argTwo)
         self.d.update({"temp0": "5",
                        "temp1": "6",
@@ -73,12 +99,15 @@ class codeWriter(object):
                        "or": "|"})
 
     def writeCmd(self, parser):
+        #set commandType, argOne, argTwo to be same with those of parser object
         self.commandType = parser.commandType
         self.argOne = parser.argOne
         self.argTwo = parser.argTwo
+        #add raw command as comment at the top of block of assembly command
+        #for debugging purpose
         self.asmCmd = ["// " + parser.raw]
 
-        if self.commandType == "ari":
+        if self.commandType == "arithmetic":
             self.writeArithmetic()
         elif self.commandType == "push":
             self.writePush()
@@ -86,8 +115,8 @@ class codeWriter(object):
             self.writePop()
 
     def writeArithmetic(self):
-        #method that will call different code generation methods according to
-        #arithmetic type
+        #method that calls different code generation methods according to
+        #individual arithmetic type
         if self.argOne in {"add", "sub"}:
             self.add_sub()
         elif self.argOne in {"eq", "lt", "gt"}:
@@ -99,7 +128,7 @@ class codeWriter(object):
 
     def writePush(self):
         #generate asm code for push command
-        #case constant
+        #case "constant"
         if self.argOne == "constant":
             self.asmCmd.extend(["@" + self.argTwo,
                                 "D=A",
@@ -108,7 +137,7 @@ class codeWriter(object):
                                 "M=D",
                                 "@SP",
                                 "M=M+1"])
-
+        #case "local", "argument", "this", "that"
         elif self.argOne in {"local", "argument", "this", "that"}:
             self.asmCmd.extend(["@" + self.argTwo,
                                 "D=A",
@@ -123,7 +152,7 @@ class codeWriter(object):
                                 "D=A",
                                 "@" + self.d[self.argOne],
                                 "M=M-D"])
-
+        #case "temp", "pointer", "static"
         elif self.argOne in {"temp", "pointer", "static"}:
             self.asmCmd.extend(["@" + self.d[self.argOne + self.argTwo],
                                 "D=M",
@@ -134,7 +163,7 @@ class codeWriter(object):
 
     def writePop(self):
         #generate asm code for pop command
-        #case "local" or "argument"
+        #case "local", "argument", "this", "that"
         if self.argOne in {"local", "argument", "this", "that"}:
             self.asmCmd.extend(["@" + self.argTwo,
                                 "D=A",
@@ -150,7 +179,7 @@ class codeWriter(object):
                                 "D=A",
                                 "@" + self.d[self.argOne],
                                 "M=M-D"])
-
+        #case "temp", "pointer", "static"
         elif self.argOne in {"temp", "pointer", "static"}:
             self.asmCmd.extend(["@SP",
                                 "AM=M-1",
@@ -159,7 +188,7 @@ class codeWriter(object):
                                 "M=D"])
 
     def add_sub(self):
-        #generate asm code for add or sub command
+        #generate asm code for "add" or "sub" command
         self.asmCmd.extend(["@SP",
                             "AM=M-1",
                             "D=M",
@@ -180,7 +209,7 @@ class codeWriter(object):
                             "A=M-1",
                             "M=0",
                             "(CONTINUE" + str(self.continue_counter) + ")"])
-
+        #increment continue_counter by 1 for generating next continue label
         self.continue_counter += 1
 
     def neg_not(self):
@@ -199,10 +228,12 @@ class codeWriter(object):
                             "M=D"])
 
     def close(self):
+        #close output file
         self.outf.close()
 
 class vmTranslator(object):
-
+    #vmTranslator class pieces parser and codeWriter together, constructor takes
+    #the file path and file name to be translated, constructs parser and codeWriter
     def __init__(self, inputName):
         self.inputName = inputName
         self.parser = parser()
@@ -216,10 +247,13 @@ class vmTranslator(object):
         else:
             with inf:
                 for row in inf:
+                    #skip if the input line is comment or empty line
                     if row[:2] == "//" or row == "\r\n":
                         continue
+                    #translate vm command into assembly command
                     self.parser.generate(row.replace("\r\n", ""))
                     self.codeWriter.writeCmd(self.parser)
+                    #write assembly commands into output file
                     for cmd in self.codeWriter.asmCmd:
                         self.codeWriter.outf.write(cmd + "\n")
             self.codeWriter.close()
